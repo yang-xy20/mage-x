3b4c57dc679bd7d6798f8e0ab82b7e614bdac56e
yang-xy20 <13760800180@163.com>
2022-09-28 05:48:30 +0000

    add_drone

---
+++ onpolicy/scripts/train/train_drone.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+import sys
+import os
+import wandb
+import socket
+import setproctitle
+import numpy as np
+from pathlib import Path
+import torch
+from onpolicy.config import get_config
+from onpolicy.envs.gym_pybullet_drones.gym_pybullet_drones.envs.multi_agent_rl.NavAviary import NavAviary
+from onpolicy.envs.env_wrappers import SubprocVecEnv, DummyVecEnv
+
+"""Train script for drones."""
+
+def make_train_env(all_args):
+    def get_env_fn(rank):
+        def init_env():
+            if all_args.env_name == "Drone":
+                env = NavAviary(all_args)
+            else:
+                print("Can not support the " +
+                      all_args.env_name + "environment.")
+                raise NotImplementedError
+            env.seed(all_args.seed + rank * 1000)
+            return env
+        return init_env
+    if all_args.n_rollout_threads == 1:
+        return DummyVecEnv([get_env_fn(0)])
+    else:
+        return SubprocVecEnv([get_env_fn(i) for i in range(all_args.n_rollout_threads)])
+
+
+def make_eval_env(all_args):
+    def get_env_fn(rank):
+        def init_env():
+            if all_args.env_name == "Drone":
+                env = NavAviary(all_args)
+            else:
+                print("Can not support the " +
+                      all_args.env_name + "environment.")
+                raise NotImplementedError
+            env.seed(all_args.seed * 50000 + rank * 10000)
+            return env
+        return init_env
+    if all_args.n_eval_rollout_threads == 1:
+        return DummyVecEnv([get_env_fn(0)])
+    else:
+        return SubprocVecEnv([get_env_fn(i) for i in range(all_args.n_eval_rollout_threads)])
+
+
+def parse_args(args, parser):
+    parser.add_argument('--scenario_name', type=str,
+                        default='2d-navigation', help="Which scenario to run on")
+    parser.add_argument('--num_agents', type=int,
+                        default=2, help="number of players")
+    parser.add_argument('--freq', type=int,
+                        default=240, help="number of players")
+    parser.add_argument('--agg_phy_steps', type=int,
+                        default=1, help="number of players")
+    parser.add_argument('--n_radius', type=float,
+                        default=np.inf, help="number of players")
+    parser.add_argument('--controller_num_agents', type=int,
+                        default=1, help="number of players")
+    parser.add_argument("--use_new_loss", action="store_true", 
+                        default=False, 
+                        help="by default False. If True, use_new_policy_loss")
+    parser.add_argument("--use_mlp_encoder", action="store_true", 
+                        default=False, 
+                        help="by default False. If True, use_mlp_encoder")
+    parser.add_argument("--use_macro", action="store_true", 
+                        default=False, 
+                        help="by default False. If True, use_macro_micro")
+    parser.add_argument("--use_gnn", action="store_true", 
+                        default=False, 
+                        help="by default False. If True, use_gnn select goals")
+    parser.add_argument("--use_exe_gnn", action="store_true", 
+                        default=False, 
+                        help="by default False. If True, use_gnn select actions")
+    parser.add_argument('--step_difference', type=int, default=2, help="local_num_step")
+    
+    all_args = parser.parse_known_args(args)[0]
+
+    return all_args
+
+
+def main(args):
+    parser = get_config()
+    all_args = parse_args(args, parser)
+
+    if all_args.algorithm_name == "rmappo":
+        assert (all_args.use_recurrent_policy or all_args.use_naive_recurrent_policy), ("check recurrent policy!")
+    elif all_args.algorithm_name == "mappo":
+        assert (all_args.use_recurrent_policy == False and all_args.use_naive_recurrent_policy == False), ("check recurrent policy!")
+    else:
+        raise NotImplementedError
+
+    assert (all_args.share_policy == True and all_args.scenario_name == 'simple_speaker_listener') == False, (
+        "The simple_speaker_listener scenario can not use shared policy. Please check the config.py.")
+
+    # cuda
+    if all_args.cuda and torch.cuda.is_available():
+        print("choose to use gpu...")
+        device = torch.device("cuda:0")
+        torch.set_num_threads(all_args.n_training_threads)
+        if all_args.cuda_deterministic:
+            torch.backends.cudnn.benchmark = False
+            torch.backends.cudnn.deterministic = True
+    else:
+        print("choose to use cpu...")
+        device = torch.device("cpu")
+        torch.set_num_threads(all_args.n_training_threads)
+
+    # run dir
+    run_dir = Path(os.path.split(os.path.dirname(os.path.abspath(__file__)))[
+                   0] + "/results") / all_args.env_name / all_args.scenario_name / all_args.algorithm_name / all_args.experiment_name
+    if not run_dir.exists():
+        os.makedirs(str(run_dir))
+
+    # wandb
+    if all_args.use_wandb:
+        run = wandb.init(config=all_args,
+                         project=all_args.env_name,
+                         entity=all_args.wandb_name,
+                         notes=socket.gethostname(),
+                         name=str(all_args.algorithm_name) + "_" +
+                         str(all_args.experiment_name) +
+                         "_seed" + str(all_args.seed),
+                         group=all_args.scenario_name,
+                         dir=str(run_dir),
+                         job_type="training",
+                         reinit=True)
+    else:
+        if not run_dir.exists():
+            curr_run = 'run1'
+        else:
+            exst_run_nums = [int(str(folder.name).split('run')[1]) for folder in run_dir.iterdir() if str(folder.name).startswith('run')]
+            if len(exst_run_nums) == 0:
+                curr_run = 'run1'
+            else:
+                curr_run = 'run%i' % (max(exst_run_nums) + 1)
+        run_dir = run_dir / curr_run
+        if not run_dir.exists():
+            os.makedirs(str(run_dir))
+
+    setproctitle.setproctitle(str(all_args.algorithm_name) + "-" + \
+        str(all_args.env_name) + "-" + str(all_args.experiment_name) + "@" + str(all_args.user_name))
+
+    # seed
+    torch.manual_seed(all_args.seed)
+    torch.cuda.manual_seed_all(all_args.seed)
+    np.random.seed(all_args.seed)
+
+    # env init
+    envs = make_train_env(all_args)
+    eval_envs = make_eval_env(all_args) if all_args.use_eval else None
+    num_agents = all_args.num_agents
+
+    config = {
+        "all_args": all_args,
+        "envs": envs,
+        "eval_envs": eval_envs,
+        "num_agents": num_agents,
+        "device": device,
+        "run_dir": run_dir
+    }
+
+    # run experiments
+    if all_args.share_policy:
+        if all_args.use_macro:
+            from onpolicy.runner.shared.drone_hrunner import DroneHRunner as Runner
+        else:
+            from onpolicy.runner.shared.drone_runner import DroneRunner as Runner
+    else:
+        from onpolicy.runner.separated.drone_runner import DroneRunner as Runner
+
+    runner = Runner(config)
+    runner.run()
+    
+    # post process
+    envs.close()
+    if all_args.use_eval and eval_envs is not envs:
+        eval_envs.close()
+
+    if all_args.use_wandb:
+        run.finish()
+    else:
+        runner.writter.export_scalars_to_json(str(runner.log_dir + '/summary.json'))
+        runner.writter.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
