c692369af72cb17927f34ccb77487a199073bc7b
yang-xy20 <13760800180@163.com>
2022-10-11 06:01:00 +0000

    fix

--- onpolicy/scripts/train_mpe.sh
+++ onpolicy/scripts/train_mpe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 env="MPE"
-scenario="push_ball"  # simple_speaker_listener # simple_reference
+scenario="simple_spread"  # simple_speaker_listener # simple_reference
 num_landmarks=50
 num_agents=50
 algo="rmappo"
@@ -13,8 +13,7 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=3 python train/train_mpe.py --env_name ${env} --algorithm_name ${algo} \
     --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} \
-    --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 \
-    --ctl_num_mini_batch 50 --exe_num_mini_batch 60 --episode_length 61 --num_env_steps 2000000000 --ppo_epoch 3 --use_ReLU --gain 0.01 \
-    --lr 7e-4 --critic_lr 7e-4 --wandb_name "mapping" --user_name "yang-xy20" --use_macro \
-    --step_difference 60 --controller_num_agents 1 --hidden_size 32 --use_wandb --use_exe_gnn
+    --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 64 \
+    --num_mini_batch 60 --episode_length 60 --num_env_steps 2000000000 --ppo_epoch 3 --use_ReLU --gain 0.01 \
+    --lr 7e-4 --critic_lr 7e-4 --wandb_name "mapping" --user_name "yang-xy20"
 done
